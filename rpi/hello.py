b2aa40a2fe3bf1d243bba1c83e7847f21b13b754
mufeez-amjad <zeefumdajma@outlook.com>
2018-11-13 18:29:47 -0500

    added chart with dummy data

--- rpi/hello.py
+++ rpi/hello.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template
-app = Flask(__name__)
- 
+
+app = Flask(__name__) 
+
+weekLabels = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+    'Friday', 'Saturday', 'Sunday'
+]
+
+timeLabels = [
+    '00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00',
+    '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30'
+]
+
+values = [
+    967.67, 1190.89, 13079.75, 1349.19,
+    2328.91, 2504.28, 2873.83
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"
+]
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    line_values = values
+    return render_template('index.html', title='Sleep Quality Over the Week', max=17000, labels=timeLabels, values=line_values)
  
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ rpi/quickstart.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1RJhe1jhH83FbOgCX6tFce6c6rfCSPM-iPrq2uelVhXQ'
+SAMPLE_RANGE_NAME = 'Sheet1!A:A'
+
+def main():
+    #gets auth for sheet id
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            print('%s' % (row[0]))
+
+if __name__ == '__main__':
+    main()
