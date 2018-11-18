bfd1bdc76ecae1af8860b91f8ca0d6f55e5fac80
mufeez-amjad <zeefumdajma@outlook.com>
2018-11-18 02:20:41 -0500

    added more cards

--- README.md
+++ README.md
@@ -11,19 +11,17 @@
 - [ ] Data Science
 
 ### Webpage:
-- [ ] Dashboard
+- [X] Dashboard
 - [ ] Tips for better sleep
-- [ ] Time in bed
-- [ ] Snoring - Yes/No + Decibels
-- [ ] Your Ideal Temperature + Humidity for Sleep 
-- [ ] Graph - Sleep Cycle
-- [ ] Graph - Sleep Quality Index over the week
+- [X] Time in bed
+- [X] Your Ideal Temperature + Humidity for Sleep 
+- [X] Graph - Sleep Cycle
+- [X] Graph - Sleep Quality Index over the week
 - [ ] Graph - Wake up time + avg wake time
 - [ ] Graph - Sleep time + avg sleep time
-- [ ] Graph - Sleep Quality vs Temperature
 
 
-  <!-- TODO: Straight Stats - Time in Bed, Snoring - Yes/No + Decibels, Average Time Slept -->
+  <!-- TODO: Straight Stats - Time in Bed Average Time Slept -->
 
   <!-- TODO: Graphs - Sleep Cycle per night, Sleep Quality % per day of week -->
 
--- rpi/hello.py
+++ rpi/hello.py
@@ -9,6 +9,13 @@ weekLabels = [
     'Friday', 'Saturday', 'Sunday'
 ]
 
+tips = [
+    'Increase bright light exposure during the day', 'Reduce blue light exposure in the evening', 'Don\'t consume caffeine late in the day', 'Reduce irregular or long daytime naps',
+    'Try to sleep and wake at consistent times', 'Take a melatonin supplement', 'Don\'t drink alcohol', 'Optimize your bedroom environment', 'Set your bedroom temperature',
+    'Don\'t eat late in the evening', 'Relax and clear your mind in the evening', 'Take a relaxing bath or shower', 'Rule out a sleep disorder', 'Get a comfortable bed, mattress and pillow',
+    'Exercise regularly - but not before bed', 'Don\'t drink any liquids before bed'
+]
+
 timeLabels2 = [
     '00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00',
     '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30'
@@ -59,7 +66,7 @@ def hello():
             condensedNightData.append(averageForInterval)
             averageForInterval = 0
 
-    return render_template('index.html', sleepGraphLabels=timeLabels, values=condensedNightData, timeWeekLabels=weekLabels, averageTime=avgTime, timeGraphData=thisWeekTime)
+    return render_template('index.html', sleepGraphLabels=timeLabels, values=condensedNightData, timeWeekLabels=weekLabels, averageTime=avgTime, timeGraphData=thisWeekTime, tipsArr=tips)
  
 if __name__ == "__main__":
     app.run(debug=True)
