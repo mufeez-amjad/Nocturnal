e139609bc964880aeef33591a25fed8720516a73
Ishan Chho <cargokhakis@gmail.com>
2018-11-19 10:58:07 -0500

    authenticate gspread for write

--- rpi/read.py
+++ rpi/read.py
@@ -1,9 +1,17 @@
 import serial
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 ser = serial.Serial('/dev/cu.usbmodem14101', 9600)
 
+credentials = ServiceAccountCredentials.from_json_keyfile_name('Nocturnal-7d790916f958.json', scope)
+gc = gspread.authorize(credentials)
+
+def formatData(data):
+    return data
+
 def readData():
     while True:
-        print(ser.readline())
+        print(formatData(ser/readline()))
 
 readData()
