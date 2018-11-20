006ade6993f378fe1070f7c6e1b894d2e032fe11
Ishan Chho <cargokhakis@gmail.com>
2018-11-20 01:14:39 -0500

    update read

--- rpi/read.py
+++ rpi/read.py
@@ -8,68 +8,62 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 credentials = ServiceAccountCredentials.from_json_keyfile_name('Nocturnal-7d790916f958.json', scope)
 gc = gspread.authorize(credentials)
 
-def formatLine(data):
-    output = str(data)
-    return output[2:len(output)-5]
-
-def getType(data):
-    return data[:data.find(":")]
-
-def getValue(data):
-    return int(float(data[data.find(":")+1:]))
-
-second = 0
-activity = 0
-temperature = 0
-humidity = 0
-light = 500
-
-sleepsecond = 0
-wakesecond = 0
-
 sleeping = False
 
-def uploadMinute():
-    
+count = 1
 
-def uploadWakeUp():
+temp_sum = 0
+humid_sum = 0
+light_sum = 0
+previous_light = 500
+
+def formatLine(line):
+    output = str(line)
+    return output[2:len(output)-5]
+
+def getType(line):
+    return line[:line.find(":")]
+
+def getValue(line):
+    return int(float(line[line.find(":")+1:]))
+
+def uploadMinute(activity):
+    # current time
+
+def uploadWake():
+    avg_temp = int(temp_sum / count)
+    avg_humid = int(humid_sum / count)
+    avg_light = int(light_sum / count)
+    # sleep time
+    # wake up time
+    # sleep duration
 
 def readData():
     while True:
-        line = formatLine(ser.readline())
-
+        line = formatLine(ser.readLine())
         if line == "END":
-            if second % 60 == 0 and sleeping:
-                uploadMinute()
-            second += 1
-
-        type = getType(line)
-        value = getValue(line)
-
-        if type == "Activity":
-            activity += value
-        elif type == "Temperature":
-            temperature += value
-        elif type == "Humidity":
-            humidity += value
-        elif type == "Light":
-            if light > 300 and value < 300 and not sleeping:
-                # go to sleep
-                print("Going to bed...")
-                sleepsecond = second
-                sleeping = True
-            elif light < 300 and value > 300 and sleeping:
-                if second - sleepsecond > 1800: # slept for at least 30 minutes
-                    # wake up
+            count += 1
+        else:
+            type = getType(line)
+            value = getValue(line)
+            if type == "Temperature":
+                if sleeping:
+                    temp_sum += value
+            elif type == "Humidity":
+                if sleeping:
+                    humid_sum += value
+            elif type == "Light":
+                if sleeping:
+                    light_sum += value
+                if previous_light > 300 and value < 300 and not sleeping:
+                    # Make sure its not day time
+                    print("Going to bed...")
+                    sleeping = True
+                elif previous_light < 300 and value > 300 and sleeping:
+                    # Make sure they were sleeping for 30+ minutes
                     print("Waking up...")
-                    wakesecond = second
                     sleeping = False
-
-
-                    sleep_duration = float(float(wakesecond - sleepsecond) / 3600)
-
-            light = value
-
-
+                previous_light = value
+            elif type == "Activity":
 
 readData()
