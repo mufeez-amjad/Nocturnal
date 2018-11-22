635e154e2f3e74604594c23137d4f90a710133c7
Ishan Chho <cargokhakis@gmail.com>
2018-11-21 20:31:37 -0500

    calculate activity per second

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -9,6 +9,10 @@ dht DHT;
 
 #define TIME 2000
 
+double absolute(double x) {
+  return x < 0 ? -x : x;
+}
+
 void setup() {
   Wire.begin();
   Wire.beginTransmission(MPU_ADDRESS);
@@ -32,7 +36,9 @@ void getLight() {
   Serial.println(lightValue);
 }
 
-void getMotion() {
+double max_activity = 0;
+
+void calcMotion() {
   Wire.beginTransmission(MPU_ADDRESS);
   Wire.write(0x3B); //starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -44,17 +50,35 @@ void getMotion() {
   int gyroX = Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   int gyroY = Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   int gyroZ = Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("accelX:"); Serial.println(accelX);
-  Serial.print("accelY:"); Serial.println(accelY);
-  Serial.print("accelZ:"); Serial.println(accelZ);
-  Serial.print("gyroX:"); Serial.println(gyroX);
-  Serial.print("gyroX:"); Serial.println(gyroY);
-  Serial.print("gyroX:"); Serial.println(gyroZ);
+
+  if (accelX > 25100) accelX = 25100;
+  else if (accelX < 9100) accelX = 9100;
+  if (gyroZ > 3300) gyroZ = 3300;
+  else if (gyroZ < -3700) gyroZ = -3700;
+  if (gyroY > 3500) gyroY = 3500;
+  else if (gyroY < -3500) gyroY = -3500;
+  
+  double activity = ((double(absolute(accelX - 17100)) / 8000) + (double(absolute(gyroZ + 200)) / 3500) + (double(absolute(gyroY)) / 3500)) / 3;
+  if (activity > max_activity) max_activity = activity;
 }
 
+void getMotion() {
+  Serial.print("Activity:");
+  Serial.println(max_activity);
+}
+
+int ms = 0;
+
 void loop() {
-  getHumidTemp();
-  getLight();
-  getMotion();
-  delay(TIME);
+  if (ms == 100) {
+    getHumidTemp();
+    getLight();
+    getMotion();
+    Serial.println("END");
+    max_activity = 0;
+    ms = 0;
+  }
+  calcMotion();
+  ms++;
+  delay(10);
 }
